f39ce1824d225c2109a351a8e6091c7754dd20ef
hunkid <273946286@qq.com>
2016-11-13 14:05:59 +0800

    finished ^ ^

--- task17/task17.js
+++ task17/task17.js
@@ -43,15 +43,27 @@ var aqiSourceData = {
 };
 
 // 用于渲染图表的数据
+/*最终charData形式为
+  charData = {
+  "北京": {
+    day:[10,10,...],
+    week:[10,10,...],
+    month:[10,10,...]
+  }
+  "广州": {
+   ...
+  }
+  }
+*/
 var chartData = {};
 
 // 记录当前页面的表单选项
 var pageState = {
-  nowSelectCity: -1,
+  nowSelectCity: "北京",
   nowGraTime: "day"
 }
 
-var monthdays = [31,30,31,28,31,30,31,31,30,31,30,31];//1-12每月的天数
+var monthdays = [31,29,31,30,31,30,31,31,30,31,30,31];//1-12每月的天数
 
 
 function getSelectOption(obj){
@@ -77,7 +89,35 @@ function getRadioVal(obj){
  * 渲染图表
  */
 function renderChart() {
+  var chart = document.getElementById("chart");
+  var chartpart =document.createElement("div");
+  chart.width = "100%";
+  
+  var col_width = {
+    "day": "8px",
+    "week": "15px",
+    "month": "22px"
+  };//用于每个柱状宽度，基于day,week,month
+  var col_height = 0;//基于每个柱状的高度
+  var col_colors = ["black","blue","yellow","red","gray"];//用于随机生成颜色
 
+  var num = chartData[pageState.nowSelectCity][pageState.nowGraTime];//用于生成柱状图的数据
+  chartpart.style.width = (parseInt(col_width[pageState.nowGraTime])+2)* num.length+"px";
+  chartpart.style.margin = "50px auto";
+  chart.appendChild(chartpart);
+  var temp;//用于生成柱状条
+  //pageState.nowSelectCity ="北京";//for test
+  
+  for (var i = 0; i < num.length; i++) {
+     temp = document.createElement("div");
+     temp.style.height = Math.round(num[i])+"px";
+     temp.style.width = col_width[pageState.nowGraTime];
+     temp.style.backgroundColor = col_colors[Math.floor(Math.random()*5)];
+     temp.style.display = "inline-block";
+     temp.style.border = "1px white solid";
+     chartpart.appendChild(temp);
+  }
+  
 }
 
 /**
@@ -86,17 +126,13 @@ function renderChart() {
 function graTimeChange() {
   // 确定是否选项发生了变化
   // 设置对应数据
-  
   var preGraTime = pageState.nowGraTime;
   var gratime = document.getElementsByName("gra-time");
   getRadioVal(gratime);
-  alert(pageState.nowGraTime);
   if (preGraTime !== pageState.nowGraTime ) {
       // 调用图表渲染函数
-      alert("调用图表渲染函数");
+      renderChart();
   }
-  
-
 
 }
 
@@ -110,9 +146,8 @@ function citySelectChange() {
   var city = document.getElementById("city-select");
   var preCity = pageState.nowSelectCity;
   getSelectOption(city);
-  alert(pageState.nowSelectCity);
   if (preCity !== pageState.nowSelectCity) {
-    alert("调用图表渲染函数");
+    renderChart();
   }
 }
 
@@ -144,15 +179,17 @@ function initAqiChartData() {
   // 将原始的源数据处理成图表需要的数据格式
   // 处理好的数据存到 chartData 中
   //处理好存放在chartData中
-  for(var elem in aqiSourceData) { //首先遍历对象属性(城市)，每个属性值都是键值
+  
+  for(var elem in aqiSourceData) { //首先遍历对象属性(城市)，每个elem都是城市
     var aqi = []; //每个城市每一天的aqi值存在这个数组里
     for(var num in aqiSourceData[elem]) {//每个属性，也即每个城市
       aqi[aqi.length] = aqiSourceData[elem][num];
     }
-    chartData[elem] = {}; //每个城市对应一个对象
-    chartDate[elem].date = aqi; //date对应每天
+
+    chartData[elem] = new Object(); //每个城市对应一个对象
+    chartData[elem].day = aqi; //date对应每天
     
-    var dt = new Array("2016-01-01");
+    var dt = new Date("2016-01-01");
     var dt_w = dt.getDay();//返回周几，1,2,3...
     var dt_m = dt.getMonth(); //返回几月份
     var dt_dt = dt.getDate();//返回是每月第几号
@@ -162,7 +199,7 @@ function initAqiChartData() {
     var sum_m = 0;
     var acount_w = 0;
     var acount_m = 0;
-    for (var i = 0; i < 92; i++) {
+    for (var i = 0; i < 91; i++) {
       sum_w += aqi[i];
       dt_w++;
       acount_w++;
@@ -172,7 +209,6 @@ function initAqiChartData() {
         dt_w = 1;
         acount_w = 0;
       }
-
       
       sum_m += aqi[i];
       dt_dt++;
@@ -185,18 +221,19 @@ function initAqiChartData() {
         if (dt_m === 13) {dt_m = 1;}
         acount_m = 0;
       }
-      
-    }
+    }//遍历92天结束
     if (acount_w !== 0) {
+      
       wk[wk.length] = sum_w / acount_w;
     }
     if (acount_m !== 0) {
       mt[mt.length] = sum_m / acount_m;
     }
 
-    chartDate[elem].week = wk;
-    chartData[elem].month = mt;
+      chartData[elem].week = wk;
+      chartData[elem].month = mt;
   }
+  
 }
 
 /**
@@ -204,7 +241,7 @@ function initAqiChartData() {
  */
 function init() {
   window.onload = function(){
-  initGraTimeForm()；
+  initGraTimeForm();
   initCitySelector();
   initAqiChartData();
   };
